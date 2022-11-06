f6620f4bde8831c9487898d2ba93e3eaa7000b9f
ProkopevKirilliv122 <91113287+ProkopevKirilliv122@users.noreply.github.com>
2022-11-07 03:03:15 +0700

    Add files via upload

---
+++ obj/test/func-test.d
@@ -0,0 +1,5 @@
+obj/test/func-test.o: test/func-test.c ctests/ctest.h \
+ src/libconverter/converter.h src/libconverter/strings.h
+ctests/ctest.h:
+src/libconverter/converter.h:
+src/libconverter/strings.h:
---
+++ obj/test/test.d
@@ -0,0 +1,2 @@
+obj/test/test.o: test/test.c ctests/ctest.h
+ctests/ctest.h:
